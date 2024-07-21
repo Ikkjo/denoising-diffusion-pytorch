43170cf501c73c2e65c91aae7f55a95e38e7bce9
Ikkjo <ilija.kalinic@gmail.com>
2024-07-21 14:12:14 +0200

    Add image generation script

---
+++ img_gen.py
@@ -0,0 +1,16 @@
+import torch
+from unet import UNet
+from ddpm import Diffusion
+from utils import plot_images
+
+def main():
+    device = "cuda"
+    model = UNet().to(device)
+    ckpt = torch.load("unconditional_ckpt.pt")
+    model.load_state_dict(ckpt)
+    diffusion = Diffusion(img_size=64, device=device)
+    x = diffusion.sample(model, n=16)
+    plot_images(x)
+
+if __name__ == "__main__":
+    main()
